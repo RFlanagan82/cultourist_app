ea001e7d7116bb1c006bc5b3a205a6143a35b28b
Aubrey Polk <aubstacle3@gmail.com>
2020-10-13 15:23:18 -0400

    initial handlebar routes

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const exphbs = require("express-handlebars");
 const handlebars = require("handlebars");
 const {
-    allowInsecurePrototypeAccess,
-  } = require("@handlebars/allow-prototype-access");
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 const db = require("./models");
 
 const PORT = process.env.PORT || 8080;
@@ -15,17 +15,16 @@ app.use(express.json());
 
 //Handlebars Middleware
 app.engine(
-    "handlebars",
-    exphbs({
-      defaultLayout: "main",
-      handlebars: allowInsecurePrototypeAccess(handlebars),
-    })
-  );
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+  })
+);
 app.set("view engine", "handlebars");
 
 // Static directory
 app.use(express.static("public"));
-
 
 // Routes Test
 //=============================================================
@@ -40,16 +39,16 @@ app.use(express.static("public"));
 //     res.json({
 //       success: true,
 //     });
-// }); 
+// });
 //=============================================================
 
 //ROUTES
 //Plug in the require
-
+var routes = require("./routes/api-routes")(app);
 
 //Event Listener on PORT
-db.sequelize.sync().then(function() {
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
+db.sequelize.sync().then(function () {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
