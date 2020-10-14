86def1c69f363ca0085aab8ccedfaf1f1bfa9f80
TDWalsh <thomas.walsh736@gmail.com>
2020-10-14 15:24:00 -0400

    pulled both belongsto methods into the single associate function on the posts.js file and targeted the user.full-name in country.handlebars

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-let capital = ""
-let currency = ""
-let currencySymbol = ""
-let flag = ""
-let language = ""
-let languageNative = ""
-let nameNative = ""
-let population = ""
-let domain = ""
-let country = "china"
-
-
-=======
->>>>>>> dab6308ee1758d9845fa33cc5f141d74782e0ab3
 $(document).ready(function () {
   $(".dropdown-trigger").dropdown();
   $('select').formSelect();
