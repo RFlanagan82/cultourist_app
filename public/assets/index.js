47dde3fa37e8ed481b016642a1f09baf9084d77f
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-10-16 16:06:16 -0400

    added validation to all forms

--- public/assets/index.js
+++ public/assets/index.js
@@ -4,12 +4,17 @@ $(document).ready(function () {
 
   $("#newUserForm").on("submit", (event) => {
     event.preventDefault();
-    const newName = { full_name: $("#newUser").val() };
-    console.log("submitted");
-    $.post("/api/create-user", newName).then((data) => {
-      console.log(data);
-      location.reload();
-    });
+    const newUser = $("#newUser").val();
+    if (newUser === "") {
+      $("#newUserError").text("User name cannot be blank.");
+    } else {
+      const newName = { full_name: $("#newUser").val() };
+      console.log("submitted");
+      $.post("/api/create-user", newName).then((data) => {
+        console.log(data);
+        location.reload();
+      });
+    }
   });
 
   $(".deleteBtn").on("click", function (event) {
@@ -28,28 +33,43 @@ $(document).ready(function () {
       .catch((err) => console.log(err));
   });
 
-  $("#submitBtn").on("click", function (event) {
+  $("#postUpdate").on("submit", function (event) {
     event.preventDefault();
-    let id = $(this).attr("data-id");
-    let userId = $(this).attr("data-user");
-    const updatedPost = {
-      title: $("#title").val(),
-      body: $("#body").val(),
-    };
-    $.ajax({
-      type: "PUT",
-      url: "/update/" + id,
-      data: updatedPost,
-    })
-      .then((data) => {
-        console.log(data);
-        location.replace("/manage/user/" + userId);
+    let id = $("#updateSubmitBtn").attr("data-id");
+    let userId = $("#updateSubmitBtn").attr("data-user");
+    let title = $("#title").val();
+    let body = $("#body").val();
+
+    if (title === "") {
+      $("#updateTitleError").text("Title cannot be blank.");
+    }
+    if (body === "") {
+      $("#updateBodyError").text("Insight cannot be blank.");
+    } else {
+      const updatedPost = {
+        title: title,
+        body: body,
+      };
+      $.ajax({
+        type: "PUT",
+        url: "/update/" + id,
+        data: updatedPost,
       })
-      .catch((err) => console.log(err));
+        .then((data) => {
+          console.log(data);
+          location.replace("/manage/user/" + userId);
+        })
+        .catch((err) => console.log(err));
+    }
   });
 
-  $("#post-id").on("submit", function (event) {
+  $("#newPost").on("submit", function (event) {
     event.preventDefault();
+    $("#userError").text("");
+    $("#countryError").text("");
+    $("#categoryError").text("");
+    $("#titleError").text("");
+    $("#bodyError").text("");
     // console.log(this);
     // console.log($("#category option:selected").text());
 
@@ -58,18 +78,35 @@ $(document).ready(function () {
     const country = $("#country option:selected").val();
     const title = $("#title").val();
     const insight = $("#body").val();
+    console.log(category);
 
-    const newPost = {
-      title: title,
-      body: insight,
-      category: category,
-      CountryId: country,
-      UserId: name,
-    };
-    console.log(newPost);
-    $.post("/api/newpost", newPost).then(function (results) {
-      console.log(results);
-      location.reload();
-    });
+    if (name === "") {
+      $("#userError").text("You must a pick user.");
+    }
+    if (country === "") {
+      $("#countryError").text("You must a pick country.");
+    }
+    if (category === "Choose a Category") {
+      $("#categoryError").text("You must a pick category.");
+    }
+    if (title === "") {
+      $("#titleError").text("Title cannot be blank.");
+    }
+    if (insight === "") {
+      $("#bodyError").text("Insight cannot be blank.");
+    } else {
+      const newPost = {
+        title: title,
+        body: insight,
+        category: category,
+        CountryId: country,
+        UserId: name,
+      };
+      console.log(newPost);
+      $.post("/api/newpost", newPost).then(function (results) {
+        console.log(results);
+        location.reload();
+      });
+    }
   });
 });
